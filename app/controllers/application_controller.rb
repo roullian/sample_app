e53dfbe7ec37eaa65b06fc36c4e5f446842aa546
Roullian <roullian2@gmail.com>
2017-11-25 13:34:19 +0100

    v1 Pages statiques

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-  def hello
-  	render html: "Hola, mundo!"
-  end
-
-  def goodbye
-  	render html: "Goodbye"
-  end
+  
 end
 
